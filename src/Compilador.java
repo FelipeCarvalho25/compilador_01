34df2360d1df77749c48fd5d00f1d56f47c9c5ee
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-03-22 22:03:37 -0300

    Inicio do projeto.

---
+++ src/Compilador.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Compilador {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new Tela().telaprincipal);
+    }
+}
---
+++ src/Tela.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Tela {
+    private JPanel tela_principal;
+    private JTextPane textPane1;
+}
