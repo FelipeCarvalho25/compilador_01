d5913b1d6f32a403255e19e96a4ae7c35aa17dde
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-03-22 22:13:45 -0300

    Inicio do projeto.

--- src/Compilador.java
+++ src/Compilador.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 public class Compilador {
     public static void main(String[] args) {
         JFrame frame = new JFrame("App");
-        frame.setContentPane(new Tela().telaprincipal);
+        frame.setContentPane(new Tela().getTela_principal());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- src/Tela.java
+++ src/Tela.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
 
 public class Tela {
+
     private JPanel tela_principal;
-    private JTextPane textPane1;
+    private JPanel area_edicao;
+    private JPanel area_mensagem;
+    private JEditorPane editor_texto;
+
+
+    public JPanel getTela_principal(){
+        return this.tela_principal;
+    }
 }
