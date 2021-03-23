9bc8b502abd1fedc374971309e5a14dd574ef2db
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-03-23 00:04:31 -0300

    Segunda fase

--- src/Compilador.java
+++ src/Compilador.java
@@ -2,7 +2,17 @@ import javax.swing.*;
 
 public class Compilador {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("App");
+        JMenuBar menu = new JMenuBar();
+        JMenu arquivo = new JMenu("Arquivo");
+        JMenu edicao = new JMenu("Edição");
+        JMenu compilacao = new JMenu("Compilação");
+
+        menu.add(arquivo);
+        menu.add(edicao);
+        menu.add(compilacao);
+
+        JFrame frame = new JFrame("Compilador");
+        frame.setJMenuBar(menu);
         frame.setContentPane(new Tela().getTela_principal());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
--- src/Tela.java
+++ src/Tela.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Tela {
 
@@ -6,8 +7,26 @@ public class Tela {
     private JPanel area_edicao;
     private JPanel area_mensagem;
     private JEditorPane editor_texto;
+    private JScrollPane mensagem_responsive;
+    private JToolBar options;
+    private JButton abrirPastaButton;
+    private JButton tesouraButton;
+    private JButton copiarButton;
+    private JButton colarButton;
+    private JButton marteloButton;
+    private JButton playButton;
+    private JButton boiaButton;
+    private JButton novoArquivoButton;
 
+    public Tela(){
+        Dimension tamanho = new Dimension();
+        tamanho.setSize(800, 500);
+        tela_principal.setPreferredSize(tamanho);
+        Dimension tamanho1 = new Dimension();
+        tamanho1.setSize(600, 150);
+        area_mensagem.setSize(tamanho1);
 
+    }
     public JPanel getTela_principal(){
         return this.tela_principal;
     }
