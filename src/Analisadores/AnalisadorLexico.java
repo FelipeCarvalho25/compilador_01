7e115a2d983130d9d828f17bbbb7cd78804c5131
Joaocarvalho25 <69583375+Joaocarvalho25@users.noreply.github.com>
2021-03-29 22:41:18 -0300

    Inicio da parte lexica com base em um trabalho passado

--- src/Analisadores/AnalisadorLexico.java
+++ src/Analisadores/AnalisadorLexico.java
@@ -5,9 +5,56 @@ public class AnalisadorLexico {
     String program = "";
 
     public String compilar(String texto){
+        String cSentenca = "";
+        Automato automato = new Automato();
         this.program = texto;
-        mensagens = "codigo funcionando";
+        if (texto.length() == 0){
+            this.mensagens = "Informe alguma sentença. \n";
+        }
+
+        char cChar = ' ';
+
+        for(int i =0; i<texto.length(); i++){
+            cChar = texto.charAt(i);
+            if (cChar == ' ' || cChar == '\n' || cChar == '\t'|| i == texto.length()-1){
+                if (i == texto.length()-1){
+                    cSentenca += cChar;
+                    cSentenca = cSentenca.trim();
+
+                    if(cSentenca.length()!=0){
+                        this.mensagens += automato.automatoExecute(cSentenca)+"\n";
+                    }
+                    cSentenca = "";
+                }else{
+                    if(cSentenca.length()!=0){
+                        this.mensagens += automato.automatoExecute(cSentenca)+"\n";
+                    }
+                    cSentenca = "";
+                }
+
+            }else{
+                cSentenca += cChar;
+            }
+        }
         return mensagens;
+    }
+    private void analisaPalavraReservada(String sentenca){
+
+    }
+    private void analisaIdentificadores(String sentenca){
+
+    }
+    private void analisaConstanteNumerica(String sentenca){
+
+    }
+    private void analisaConstanteLiteral(String sentenca){
+
+    }
+    private void analisaCComentarioLinha(String sentenca){
+
+    }
+    private void analisaCComentarioBloco(String sentenca){
+
     }
     public String getMensagens(){
         return this.mensagens;
